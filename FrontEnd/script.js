e02eb89bac87b05ebdefae728eadfc780ed8f0d9
LuckyProut <luc.garrouste@gmail.com>
2024-08-21 09:31:41 +0200

    fetch works into modal

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,24 +1,3 @@
-// fetch('http://localhost:5678/api/works/')
-// .then(response => response.json())
-// .then((data) => 
-//     displayWorks(data)
-// )
-// .catch(error => alert("Erreur : " + error));
-
-// function displayWorks(works) {
-//     let divGallery = document.getElementsByClassName("gallery")[0];
-//     works.forEach(element => {
-//         let workFigure = document.createElement("figure");
-//         let workImg = document.createElement("img");
-//         let workTitle = document.createElement("figcaption");
-//         workImg.src = element.imageUrl;
-//         workImg.alt = element.title;
-//         workTitle = element.title;
-//         divGallery.appendChild(workFigure);
-//         workFigure.append(workImg, workTitle);
-//     });
-// };
-
 // récupération des travaux
 
 async function getWorks(filter) {
@@ -36,12 +15,14 @@ async function getWorks(filter) {
             // puis si filter est défini on vient afficher les éléments de la categoryId
             const filtered = json.filter((data) => data.categoryId === filter);
             for (let i = 0; i < filtered.length; i++) {
-                Gallery(filtered[i]);
+                setGallery(filtered[i]);
+                setModalGallery(filtered[i]);
             }
         } else {
             // si aucun filtre appliqué (fonction getworks défini sur 0 plus bas) alors on affiche tout
             for (let i = 0; i < json.length; i++) {
-                Gallery(json[i]);
+                setGallery(json[i]);
+                setModalGallery(json[i]);
             }
         }
     } catch (error) {
@@ -58,12 +39,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // création de la gallerie
 
-function Gallery(data) {
+function setGallery(data) {
     const figure = document.createElement("figure");
     figure.innerHTML = `<img src="${data.imageUrl}" alt="${data.title}">
     <figcaption>${data.title}</figcaption>`;
-    document.querySelector(".gallery").append(figure);
+    document.querySelector(".gallery").append(figure);   
 }
+
+function setModalGallery(data) {
+    const figure = document.createElement("figure");
+    figure.innerHTML = `<img src="${data.imageUrl}" alt="${data.title}">`;
+    document.querySelector(".modalGalleryContent").append(figure); 
+}
+
 
 
 // récupération des catégories 
